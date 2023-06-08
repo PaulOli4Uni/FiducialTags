0d896f2b4c1e486765ebd0479bae4d112612cdfe
PaulOli4Uni <21753492@sun.ac.za>
2023-06-08 15:04:23 +0200

    Loading Excel file finished. Started run simulation commands

--- sim_main.py
+++ sim_main.py
@@ -1,41 +1,53 @@
 """
-Parameters = {"world_file", "movement_path", "video_name", "gz_pose_file", "vid_pose_file", "models", "lights"}
+
 
 """
+import sys
 import time
-
 import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 import subprocess
 import os
+from dotenv import load_dotenv
+import signal
 
 headers = {'Parameter', 'Info', 'Additional_Info'}
-parameters = {"world_file", "movement_path", "video_name", "gz_pose_file", "vid_pose_file", "models", "lights"}
-
+parameters_config = {"movement_path", "video_name", "gz_pose_file", "vid_pose_file", "cameras", "models", "lights"}
+parameters_main = {"test_files_path", "world_file"}
 @dataclass()
-class pose:
-    x: float
-    y: float
-    z: float
+class dc_pose:
+    X: float
+    Y: float
+    Z: float
     r: float
     p: float
     y: float
 
-@dataclass
-class model:
+@dataclass()
+class dc_model:
     model_name: str
-    pose: pose
+    pose: dc_pose
 
-@dataclass
-class test_config_data:
-    test_name: str
+@dataclass()
+class dc_camera:
+    camera_name: str
+    camera_pose: dc_pose
+
+@dataclass()
+class dc_test_main_data:
+    test_files_path: str
     world_file: str
+
+@dataclass()
+class dc_test_config_data:
+    test_name: str
     movement_path: str
     video_name: str
     gz_pose_file: str
     vid_pose_file: str
-    models: model
+    cameras: dc_camera
+    models: dc_model
     lights: []  # todo: Add lights data class
 
 def ImportSheet(tests_config, filename):
@@ -44,59 +56,62 @@ def ImportSheet(tests_config, filename):
     sheet_names = xls.sheet_names  # List of all the sheets in the workbook (by name)
     num_sheets = len(sheet_names) # Defines number of sheets
 
+    if num_sheets < 2:
+        print("[ERR] Two sheets or more must be present. First sheet must be called 'Main'")
+        return False
+
     # Read all sheets to a map
     sheet_to_df_map = {}
     for sheet_name in sheet_names:
         sheet_to_df_map[sheet_name] = xls.parse(sheet_name)
 
-    # Read Sheet data into dataclasses
-    for i in range(0,num_sheets):
+    # Read 'Main' sheet data
+    #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    main_data = dc_test_main_data("~/", "file")
 
-        print("[INFO] Reading sheet #" + str(i+1) + ": \'" + sheet_names[i] + "\'")
+    # Read rest of Sheet data into dataclasses
+    for i in range(1,num_sheets):
+
+        print("[INFO] Reading sheet #" + str(i) + ": \'" + sheet_names[i] + "\'")
 
         sheet = sheet_to_df_map[sheet_names[i]]
-        if not CheckAllRequiredParametersPresent(sheet):
+        if not CheckAllRequiredParametersConfigPresent(sheet):
             return False
 
 
         test_name = sheet_names[i]
 
+        camera_row_index = sheet[sheet["Parameter"] == "cameras"].index[0]
         model_row_index = sheet[sheet["Parameter"] == "models"].index[0]
         light_row_index = sheet[sheet["Parameter"] == "lights"].index[0]
         max_row = len(sheet)
 
         sheet.set_index("Parameter", inplace=True)
 
-        world_file = sheet.loc["world_file"]['Info']
-        if not CheckCorrectExtention(world_file, ".sdf"):
-            print("[ERR] World file \'" + world_file + "\' has incorrect extention")
-            return False
-
         movement_path = sheet.loc["movement_path"]['Info']
         if not CheckCorrectExtention(movement_path, ".txt"):
-            print("[ERR] Movement Path file \'" + movement_path + "\' has incorrect extention")
             return False
 
         video_name = sheet.loc["video_name"]['Info']
         if not CheckCorrectExtention(video_name, ".mp4"):
-            print("[ERR] Video file \'" + world_file + "\' has incorrect extention")
             return False
 
         gz_pose_file = sheet.loc["gz_pose_file"]['Info']
         if not CheckCorrectExtention(gz_pose_file, ".txt"):
-            print("[ERR] Gazebo Pose file \'" + gz_pose_file + "\' has incorrect extention")
             return False
 
         vid_pose_file = sheet.loc["vid_pose_file"]['Info']
         if type(vid_pose_file) == str: # Input file is of type string, thus value is given
             if not CheckCorrectExtention(vid_pose_file, ".txt"):
-                print("[ERR] Video Pose file \'" + vid_pose_file + "\' has incorrect extention")
                 return False
         else: # Make sure file value is not a number (will be used to determine if
             vid_pose_file = np.nan
             print("[INFO] Video to pose file not provided")
 
-        # Import Models and lights
+        # Import Cameras, Models and lights
+        cameras = []
+
+
         models = []
         if not ImportModels(models, sheet, model_row_index, light_row_index):
             return False
@@ -105,12 +120,12 @@ def ImportSheet(tests_config, filename):
         if not ImportLights(lights, sheet, light_row_index, max_row):
             return False
 
-        test_data = test_config_data(test_name, world_file, movement_path, video_name, gz_pose_file, vid_pose_file, models, lights)
+        test_data = dc_test_config_data(test_name, movement_path, video_name, gz_pose_file, vid_pose_file, cameras, models, lights)
         tests_config.append(test_data)
 
     return True
 
-def CheckAllRequiredParametersPresent(sheet):
+def CheckAllRequiredParametersConfigPresent(sheet):
 
     # Check that all headers are given
     if not all(header in sheet.columns for header in headers):
@@ -118,7 +133,7 @@ def CheckAllRequiredParametersPresent(sheet):
         return False
 
     # Check that all parameters are present
-    for param in parameters:
+    for param in parameters_config:
         if param not in sheet["Parameter"].values:
             print("[ERR] Parameter: \'" + param + "\' not found")
             return False
@@ -127,7 +142,29 @@ def CheckAllRequiredParametersPresent(sheet):
 
 # Returns true if filename and extension matches (thus correct extension given)
 def CheckCorrectExtention(filename, extension_type):
+    print("[ERR]: Incorrect extension found for: \'" + filename + "\'")
     return filename.lower().endswith(extension_type)
+
+def ImportCameras(cameras, sheet, start_index, end_index):
+    for i in range(start_index, end_index):
+        sheet_row = sheet.iloc[i]
+
+        camera_name = sheet_row["Info"]
+        if not CheckCorrectExtention(camera_name, ".sdf"):
+            return False
+
+        camera_pose = sheet_row["Additional_Info"]
+        pose = dc_pose(0, 0, 0, 0, 0, 0)
+        if type(camera_pose) == str: # Type is a string and pose has been provided
+            pose_list = [float(x) for x in camera_pose.split(',')]
+            if len(pose_list) != 6:
+                print("[WARN]: Incorrect number of pose variables provided for: \'" + camera_name + "\'. Pose set as [0,0,0,0,0,0]")
+            else:
+                pose = dc_pose(pose_list[0], pose_list[1], pose_list[2], pose_list[3], pose_list[4], pose_list[5])
+
+        model = dc_model(camera_name, pose)
+        cameras.append(model)
+    return True
 
 def ImportModels(models, sheet, start_index, end_index):
 
@@ -135,20 +172,20 @@ def ImportModels(models, sheet, start_index, end_index):
         sheet_row = sheet.iloc[i]
 
         model_name = sheet_row["Info"]
-        if not CheckCorrectExtention(model_name, "obj"):
+        if not CheckCorrectExtention(model_name, ".sdf"):
             print("[ERR]: Incorrect Model Extension found for: \'" + model_name + "\'")
             return False
 
         model_pose = sheet_row["Additional_Info"]
-        pose = [0, 0, 0, 0, 0, 0]
+        pose = dc_pose(0, 0, 0, 0, 0, 0)
         if type(model_pose) == str: # Type is a string and pose has been provided
             pose_list = [float(x) for x in model_pose.split(',')]
             if len(pose_list) != 6:
                 print("[WARN]: Incorrect number of pose variables provided for: \'" + model_name + "\'. Pose set as [0,0,0,0,0,0]")
             else:
-                pose = [pose_list[0], pose_list[1], pose_list[2], pose_list[3], pose_list[4], pose_list[5]]
+                pose = dc_pose(pose_list[0], pose_list[1], pose_list[2], pose_list[3], pose_list[4], pose_list[5])
 
-        model = [model_name, pose]
+        model = dc_model(model_name, pose)
         models.append(model)
 
     return True
@@ -162,14 +199,33 @@ def RunSimulation(tests_config):
     print(current_dir)
     i = 0
 
-    gazebo_sim = subprocess.Popen("gz sim " + current_dir + "/Worlds/" + tests_config[i].world_file, shell=True)
-    # gazebo_sim = subprocess.Popen("gz sim " , shell=True)
-    time.sleep(100)
+    # Start Gazebo Simulator
+    cmd_start = "gz sim " + current_dir + "/Worlds/" + tests_config[i].world_file
+    # gazebo_sim = subprocess.Popen("exec " + cmd_start, stdout=subprocess.PIPE, shell=True)
+    gazebo_sim = subprocess.Popen("exec " + cmd_start, stdout=subprocess.PIPE, shell=True)
 
+    # Give time for simulator to start
+    time.sleep(2)
+    # Test if simulator has started
+    cmd_start_test = "gz service -s /gazebo/resource_paths/get --reqtype gz.msgs.Empty --reptype gz.msgs.StringMsg_V --timeout 1000 --req \"\"" # Timout of 0.1s
+    process = subprocess.Popen(cmd_start_test, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while process.returncode == 1: # Check if error has been returned from terminal command (should be a timeout error.) If yes, wait before testing again
+        time.sleep(1)
+        process = subprocess.Popen(cmd_start_test, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Gazebo has loaded
 
-    gazebo_sim.terminate()
+    """
+    Spawning a Model
+    gz service -s /world/empty/create --reqtype gz.msgs.EntityFactory --reptype gz.msgs.Boolean --timeout 1000 --req 'sdf_filename: "/home/stb21753492/FiducialTags/Markers/DICT_4X4_50_s1000/DICT_4X4_50_s1000_id1/DICT_4X4_50_s1000_id1.sdf", name: "urd22f_model"'
+    Spawming a Camera
+    gz service -s /world/empty/create --reqtype gz.msgs.EntityFactory --reptype gz.msgs.Boolean --timeout 1000 --req 'sdf_filename: "/home/stb21753492/FiducialTags/Cameras/Cam_Basic.sdf", name: "Cam"'
+
+    """
+
 
 if __name__ == '__main__':
+
+    load_dotenv()
 
     # Setup and import data
     filename = 'Test.xlsx'
@@ -184,5 +240,5 @@ if __name__ == '__main__':
 
     # Start Running Simulations
     print("Starting Simulations")
-    RunSimulation(tests_config)
+    # RunSimulation(tests_config)
 
