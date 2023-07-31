f923c615e9b583e3596d4759540301dd875aab30
PaulOli4Uni <21753492@sun.ac.za>
2023-07-31 11:29:25 +0200

    Markers added

--- Simulations/Libraries/data_classes/dc_marker.py
+++ Simulations/Libraries/data_classes/dc_marker.py
@@ -2,11 +2,10 @@ from dataclasses import dataclass
 from Simulations.Libraries.data_classes import dc_pose
 
 dc_pose = dc_pose.pose
-
 @dataclass()
 class marker:
     marker_file: str
     pose: dc_pose
-    dictionary: str #String of Dict (not aruco.getpredefineddict ...)
+    dictionary: str  # String of Dict (not aruco.getpredefineddict ...)
     id: int
-    size: float # in m (meters)
+    size: float  # in m (meters)
